06a1dff35a907d70b90c7286795d6280a322e351
Steffy29 <stephanie.moallic@gmail.com>
2015-10-28 11:11:36 +0100

    feat(init) : finish step1 for application
    update link between app, menu and list of beers
    remove controller not used in step 1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,6 @@
 angular.module('ionicbeers')
 
 .controller('BeersCtrl', ['$scope', function($scope) {
-<<<<<<< HEAD
-    $scope.beers = [
-      {
-        "name": "Affligem Blond",
-        "description": "Affligem Blonde, the classic clear blonde abbey ale, with a gentle roundness and 6.8% alcohol. Low on bitterness, it is eminently drinkable."
-      },
-      {
-        "name": "Affligem Tripel",
-        "description": "The king of the abbey beers. It is amber-gold and pours with a deep head and original aroma, delivering a complex, full bodied flavour. Pure enjoyment! Secondary fermentation in the bottle."
-      },
-      {
-        "name": "Rochefort 8",
-        "description": "A dry but rich flavoured beer with complex fruity and spicy flavours."
-      },
-      {
-        "name": "Rochefort 10",
-        "description": "The top product from the Rochefort Trappist brewery. Dark colour, full and very impressive taste. Strong plum, raisin, and black currant palate, with ascending notes of vinousness and other complexities."
-      },
-      {
-        "name": "Chimay Rouge",
-        "description": "This Trappist beer possesses a beautiful coppery colour that makes it particularly attractive. Topped with a creamy head, it gives off a slight fruity apricot smell from the fermentation. The aroma felt in the mouth is a balance confirming the fruit nuances revealed to the sense of smell. This traditional Belgian beer is best savoured at cellar temperature "
-      }
-    ];
-  }]) 
-=======
   $scope.beers = [
     {
       "alcohol": 6.8,
@@ -56,4 +31,3 @@ angular.module('ionicbeers')
 
   $scope.orderProp = 'alcohol';
 }]) 
->>>>>>> 5f4e268... feat(step4) : add users control to order the items in the beer list
