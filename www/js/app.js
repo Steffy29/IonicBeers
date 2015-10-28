14fc77234470c5539d45b8e8dcaa38b0e9bd5c73
Steffy29 <stephanie.moallic@gmail.com>
2015-10-28 11:02:11 +0100

    feat(init) : initialize step 1 for ionic beers
    add menu to app
    add home page
    add list beers
    
    feat(init) : finish step1 for application
    
    update link between app, menu and list of beers
    remove controller not used in step 1

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicbeers', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,38 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+    .state('eventmenu', {
+      url: '/event',
+      abstract: 'true',
+      templateUrl: 'templates/menu.html'
+    })
+    .state('eventmenu.home', {
+        url: '/home',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/home.html'
+          }
+        }
+    })
+    .state('eventmenu.beers', {
+        url: '/beers',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/listBeers.html'
+          }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/event/home');
+
+
+})
+
