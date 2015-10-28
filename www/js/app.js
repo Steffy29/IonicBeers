e0aa646ae5f5cbd1719ddd1ab565a7780bdf520b
Steffy29 <stephanie.moallic@gmail.com>
2015-10-28 15:58:38 +0100

    feat(translate) : add translate method
    define languages files for application (french and english files)
    add angular-translate library
    define translate provider to translate labels
    replace static labels with dynamic translated labels

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('ionicbeers', ['ionic'])
+angular.module('ionicbeers', ['ionic','pascalprecht.translate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,7 +18,7 @@ angular.module('ionicbeers', ['ionic'])
   });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $translateProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -50,6 +50,22 @@ angular.module('ionicbeers', ['ionic'])
 
     $urlRouterProvider.otherwise('/event/home');
 
+    // Translate labels / title / menus
+    $translateProvider.useSanitizeValueStrategy('escape');
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'languages/',
+        suffix:'.json'
+    });
+    $translateProvider
+    .registerAvailableLanguageKeys(['en','fr'], {
+        'en_US': 'en',
+        'en_UK': 'en',
+        'fr_FR': 'fr',
+        'fr_BE': 'fr'
+    })
+    .determinePreferredLanguage();
+
+    $translateProvider.use();
 
 })
 
