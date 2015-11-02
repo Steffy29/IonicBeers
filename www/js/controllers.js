51659e316103061ab275545aaa42f2ab531275ae
Steffy29 <stephanie.moallic@gmail.com>
2015-11-02 21:09:50 +0100

    feat(step5) : add json definition reader
    add json beers files
    define controller for reading json files

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,9 @@
 angular.module('ionicbeers')
 
-.controller('BeersCtrl', ['$scope', function($scope) {
-    $scope.beers = [
-      {
-        "name": "Affligem Blond",
-        "description": "Affligem Blonde, the classic clear blonde abbey ale, with a gentle roundness and 6.8% alcohol. Low on bitterness, it is eminently drinkable."
-      },
-      {
-        "name": "Affligem Tripel",
-        "description": "The king of the abbey beers. It is amber-gold and pours with a deep head and original aroma, delivering a complex, full bodied flavour. Pure enjoyment! Secondary fermentation in the bottle."
-      },
-      {
-        "name": "Rochefort 8",
-        "description": "A dry but rich flavoured beer with complex fruity and spicy flavours."
-      },
-      {
-        "name": "Rochefort 10",
-        "description": "The top product from the Rochefort Trappist brewery. Dark colour, full and very impressive taste. Strong plum, raisin, and black currant palate, with ascending notes of vinousness and other complexities."
-      },
-      {
-        "name": "Chimay Rouge",
-        "description": "This Trappist beer possesses a beautiful coppery colour that makes it particularly attractive. Topped with a creamy head, it gives off a slight fruity apricot smell from the fermentation. The aroma felt in the mouth is a balance confirming the fruit nuances revealed to the sense of smell. This traditional Belgian beer is best savoured at cellar temperature "
-      }
-    ];
-  }]) 
+.controller('BeersCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('beers/beers.json').success(function(data) {
+    $scope.beers = data;
+  });
+
+  $scope.orderProp = 'alcohol';
+}]) 
