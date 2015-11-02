3139334a779c1760f209848536df73df3f2e5611
Steffy29 <stephanie.moallic@gmail.com>
2015-11-02 20:11:16 +0100

    feat(step3) : add search query and filters
    add search query and filters to the page

--- step3/www/js/controllers.js
+++ step3/www/js/controllers.js
@@ -3,22 +3,34 @@ angular.module('ionicbeers')
 .controller('BeersCtrl', ['$scope', function($scope) {
     $scope.beers = [
       {
+<<<<<<< HEAD
         "name": "Affligem Blond",
         "description": "Affligem Blonde, the classic clear blonde abbey ale, with a gentle roundness and 6.8% alcohol. Low on bitterness, it is eminently drinkable."
       },
       {
+=======
+        "alcohol": 8.5,
+>>>>>>> feat(step3) : add search query and filters
         "name": "Affligem Tripel",
         "description": "The king of the abbey beers. It is amber-gold and pours with a deep head and original aroma, delivering a complex, full bodied flavour. Pure enjoyment! Secondary fermentation in the bottle."
       },
       {
+<<<<<<< HEAD
+=======
+        "alcohol": 9.2,
+>>>>>>> feat(step3) : add search query and filters
         "name": "Rochefort 8",
         "description": "A dry but rich flavoured beer with complex fruity and spicy flavours."
       },
       {
+<<<<<<< HEAD
         "name": "Rochefort 10",
         "description": "The top product from the Rochefort Trappist brewery. Dark colour, full and very impressive taste. Strong plum, raisin, and black currant palate, with ascending notes of vinousness and other complexities."
       },
       {
+=======
+        "alcohol": 7,
+>>>>>>> feat(step3) : add search query and filters
         "name": "Chimay Rouge",
         "description": "This Trappist beer possesses a beautiful coppery colour that makes it particularly attractive. Topped with a creamy head, it gives off a slight fruity apricot smell from the fermentation. The aroma felt in the mouth is a balance confirming the fruit nuances revealed to the sense of smell. This traditional Belgian beer is best savoured at cellar temperature "
       }
