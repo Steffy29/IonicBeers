14820927bb4763acdc9ee028bf3c4e1ee33b3023
Steffy29 <stephanie.moallic@gmail.com>
2015-11-04 21:18:41 +0100

    feat(beerDetail) : add beer detail in modal view
    add beer detail in modal view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,3 +7,30 @@ angular.module('ionicbeers')
 
   $scope.orderProp = 'alcohol';
 }]) 
+
+.controller('BeerDetailCtrl', ['$scope', '$stateParams', '$http', '$ionicModal', function($scope, $stateParams, $http, $ionicModal) {
+
+  $ionicModal.fromTemplateUrl('templates/beer-detail.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  $http.get('beers/' + $stateParams.beerId + '.json').success(function(data) {
+      $scope.beer = data;
+  });
+
+}])
