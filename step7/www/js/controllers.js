4e1164e94cb6a31ed35f1fcdd9f54077863c28fa
Steffy29 <stephanie.moallic@gmail.com>
2015-11-27 15:03:38 +0100

    feat(step7) : add step7

---
+++ step7/www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('ionicbeers')
+
+.controller('BeersCtrl', ['$scope', '$http', '$ionicModal', function($scope, $http, $ionicModal) {
+  $http.get('beers/beers.json').success(function(data) {
+    $scope.beers = data;
+  });
+
+  $scope.orderProp = 'alcohol';
+
+  // Define modal to display beer detail
+  $ionicModal.fromTemplateUrl('templates/beer-detail.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.beerModal = modal;
+  });
+
+  // Load beer detail from json file and show modal
+  $scope.openModal = function(beerId) {
+    $http.get('beers/' + beerId + '.json').success(function(data) {
+      $scope.beer = data;      
+      $scope.mainImg = $scope.beer.img;
+
+      $scope.setImage = function(img) {
+        $scope.mainImg = img;
+      }
+    });
+    myBeerId = beerId;
+    $scope.beerModal.show();
+  };
+
+  // Hide modal
+  $scope.closeModal = function() {
+    $scope.beerModal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.beerModal.remove();
+  });
+}]) 
